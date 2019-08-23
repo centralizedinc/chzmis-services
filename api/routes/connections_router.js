fc06b733f06db2074ad6c670a34bcf7b653e3b89
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-08-23 10:12:10 +0800

    get all connections

--- api/routes/connections_router.js
+++ api/routes/connections_router.js
@@ -12,9 +12,20 @@ var response_helper = new ResponseHelper('GROUP')
 
 
 router
-    .route('/')
+    .route()
+    .post((req, res) => {
+        ConnectionsDao.create(req.body)
+            .then((result) => {
+                response_helper.sendGetResponse(req, res, result, null, 0)
+            }).catch((err) => {
+                response_helper.sendGetResponse(req, res, null, err, 0)
+            });
+    })
+
+router
+    .route('/:account_id')
     .get((req, res) => {
-        ConnectionsDao.findOneInMember()
+        ConnectionsDao.findOneInMember(req.query.account_id)
             .then((result) => {
                 response_helper.sendGetResponse(req, res, result, null, 0)
             }).catch((err) => {
